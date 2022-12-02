a361b767021a6f88112179936088c9d2818f2b2a
Brianrafs <brianxdgmr@gmail.com>
2022-12-02 14:39:02 -0300

    Logotipo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '../css/navbar.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import Logo from '../images/novetech_logo_branco.png'
+
 
 function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand text-light" href="#">Navbar</a>
+                <a className="navbar-brand text-light" href="#">
+                   <img className='logoBranca' src={Logo}></img>
+                </a>
                 <GiHamburgerMenu />
             </div>
         </nav>
