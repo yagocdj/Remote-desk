b0e178b12d99bd09cdda4d313e059ba5f60453bc
Yago <yagocdj@gmail.com>
2022-12-18 17:16:36 -0300

    Alinhando as bandeiras dentro dos cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import '../css/card.css';
 export default function GlassCard({id, stateName, image, abbreviation, altText}) {
     return (
         <div className="card mt-5 card-container px-4 align-self-center" id={id}>
-            <div className='card-image pt-4'>
+            <div className='card-image pt-4 d-flex justify-content-center'>
                 <img src={image} alt={altText} className='flag-image'/>
             </div>
             <div className='card-description text-center my-4'>
